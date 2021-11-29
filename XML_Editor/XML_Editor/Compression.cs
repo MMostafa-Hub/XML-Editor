a0c0371a248acb1cf6e91f750770b38949e43dcb
Mohanad <mohanadismail789@gmail.com>
2021-11-29 18:29:43 +0200

    Edited Minifying function so it doesn't corrupt data

--- XML_Editor/XML_Editor/Compression.cs
+++ XML_Editor/XML_Editor/Compression.cs
@@ -8,9 +8,27 @@ namespace XML_Editor
 {
     internal class Compression
     {
-        string Minifying(string str)
+        string Minifying(Node node)
         {
-            return str.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+            //string to store the output after removing indentations and whitespaces
+            string output = "";
+            //if the passed node is null, return an empty string
+            if (node == null) return output;
+            //base case for recursive function
+            if (node.getChildren().Count() == 0) return output += node.getData();
+
+            //recursive case
+            //since it passed the base case, then it has children. So, it is an opening tag
+            output += "<" + node.getTag() + ">";
+            //call the minifying function on each child
+            foreach (Node child in node.getChildren())
+            {
+                //append returned strings to output string
+                output += Minifying(child);
+            }
+            //after iterating on all children, add the closing tag
+            output += "</" + node.getTag() + ">";
+            return output;
         }
     }
 }
