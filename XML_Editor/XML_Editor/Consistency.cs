3be26168b23d58d9e65b82772057f0481a04dab3
Nazeeh Maged <nazeehmaged007@gmail.com>
2021-11-30 16:51:34 +0200

    find character function

--- XML_Editor/XML_Editor/Consistency.cs
+++ XML_Editor/XML_Editor/Consistency.cs
@@ -8,8 +8,27 @@ namespace XML_Editor
 {
     internal class Consistency
     {
-        static string checkConsistency(string s) { 
-            //here to contiune the code
+        static string checkConsistency(string s) {
+            Stack <string> st = new Stack<string>(); 
+
+            string output= "";
+
+            return output;
         }
+        static int findChar(string s,int index,char a) {
+            for (int i = index; i < s.Length; i++) {
+                if (a == s[i])
+                    return i;
+            }
+            return 0;  
+        }
+        static int findBetween(string s,int start, int end, char a) {
+            for (int i = start; i < end; i++) {
+                if (a == s[i])
+                    return i;
+            }
+            return 0;
+        }
+
     }
 }
