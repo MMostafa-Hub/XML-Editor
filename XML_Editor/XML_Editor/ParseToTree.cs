bfca9ea264fec8ed966ceaafa4075a49a857a2f2
Hazem1009 <68254631+Hazem1009@users.noreply.github.com>
2021-12-01 23:30:00 +0200

    Added Full implementation of Parsing To tree
    Added 3 Extraction methods for tags and data , added full implementation for ParsingtoTree

--- XML_Editor/XML_Editor/ParseToTree.cs
+++ XML_Editor/XML_Editor/ParseToTree.cs
@@ -8,62 +8,119 @@ namespace XML_Editor
 {
     internal class ParseToTree
     {
+        /*Function Description:
+         * 1-Input:XML as string after Correction
+         * 2-Ouput:Returns a reference to Root after parsing XML into our Tree structure
+         */
         public static Node ParsingToTree(string sample) 
         {
             Node current=null; //Used to Reference Nodes on the run
-            int start=0 ,end=0 ; //Indices For Substrings
-            int incrementer=0; //internal Use for looping
             string tag=null;
-            for(int i = 0; i < sample.Length;) 
+            int i = 0;
+            for (; sample[i] != '<'; i++) ;
+
+           /*1-Extracting Our Root node tag after reaching it with previous loop
+            *2-Creating new root node with Tag extracted and null data
+            *3-Setting Parent of our Root node to Null
+            */
+            string rootTag=extractOpeningTag(sample, ref i);
+            Node root = new Node(rootTag, null);
+            current = root;
+            root.setParent(null);
+
+            for(; i < sample.Length;) 
             {
                 //Closing Tag case
-                if(sample[i]=='<'&&sample[i+1]=='/')
+                if (sample[i] == '<' && sample[i + 1] == '/')
                 {
-                    //Implement Closing tag;
-                    break;
+                 
+                    tag = extractClosingTag(sample, ref i);
+
+                    if (current.getParent() == null)
+                    {
+                        //Case we reached to our root ,in this case for closing we reached end of string so we break the loop
+                        break;
+                    }
+                    else
+                    {
+                        //We didnt reach end of string so we reference back to the parent to continue adding its children.
+                        current = current.getParent();
+                        
+                    }
+
                 }
                 //Opening Tag case
-                else if(sample[i]=='<')
-                { 
-                    start=i+1;
-                    incrementer=i;
-                    while (sample[incrementer] != '>') incrementer++;
-                    end=incrementer;
-                    incrementer=0;
-                    i=end+1;
-                    tag=sample.Substring(start,end-start); //extracting tag
-                    Node node =new Node(tag,null);
-                    if (current == null)
-                    {
-                        node.setParent(null); //setting parent of our node
-                    }
-                    else 
-                    {
-                        node.setParent(current);
-                    }
-                    current=node; // Saving The current Parent
+                else if (sample[i] == '<')
+                {
+
+                    tag = extractOpeningTag(sample, ref i); //extracting tag
+                    Node node = new Node(tag, null); 
+                    node.setParent(current); //setting parent of the new node 
+                    current.addChild(node); //adding the new node into Children of current 
+                    current = node; //moving on to point to next node
                 }
                 //Case We have data
-                else if (sample[i] != '\n' && sample[i] != '\r') 
+                else if (sample[i] != '\n' && sample[i] != '\r')
                 {
-                    
-                    start = i;
-                    incrementer = i;
-                    //We loop until we have a new tag 
-                    //looping until end of string is to be fixed
-                    while (incrementer <sample.Length && sample[incrementer] != '<' && sample[incrementer] != '\n')
-                    {
-                        incrementer++;
-                    }
-                    end = incrementer;
-                    incrementer= 0;
-                    string data=sample.Substring(start,end-start); //Extracting Data 
+                    //Extracting Data
+                    string data = extractData(sample, ref i);
+                    //Seting Data to our current node while parsing
                     current.setData(data);
-                    
-                    i = end;
                 }
+                else i++;
             }
-            return current;
+            return root;
+        }
+        /*Function Description:
+         * 1-Input:XML string after correction,pass by reference to our current index of XML string
+         * 2-Output:Returns a string OF extracted Opening tag
+         */
+        public static string extractOpeningTag(string sample,ref int i)
+        {
+            string tag = null;
+            int start = i + 1;
+            int incrementer = i;//temp variable to keep i not changed
+            while (sample[incrementer] != '>') incrementer++; //loop until we reach closing angle bracket
+            int end = incrementer;
+            incrementer = 0;
+            i = end + 1;    //make i points to char after >
+            tag=sample.Substring(start,end-start); //extract tag
+            return tag;
+        }
+        /*Function Description:
+         * 1-Input:XML string after correction,pass by reference to our current index of XML string
+         * 2-Output:Returns a string OF extracted data
+         */
+        public static string extractData(string sample, ref int i)
+        {
+            string data = null;
+            int start = i;
+            int incrementer = i;//temp variable to keep i not changed
+            while (incrementer < sample.Length && sample[incrementer] != '<' && sample[incrementer] != '\n')
+            {
+                incrementer++;
+            }
+            int end = incrementer;
+            incrementer = 0;
+            data=sample.Substring(start,end-start);
+            i = end;
+            return data;
+        }
+        /*Function Description:
+         * 1-Input:XML string after correction,pass by reference to our current index of XML string
+         * 2-Output:Returns a string OF extracted closing tag
+         */
+        public static string extractClosingTag(string sample, ref int i)
+        {
+            string tag = null;
+            int start = i + 2;  //To skip < and /
+            int incrementer = i; //temp variable to keep i not changed 
+            while (sample[incrementer] != '>') incrementer++;
+            int end = incrementer;
+            incrementer = 0;
+            i = end + 1;
+            tag = sample.Substring(start, end - start); //extracting the closing tag
+            return tag;
         }
     }
 }
