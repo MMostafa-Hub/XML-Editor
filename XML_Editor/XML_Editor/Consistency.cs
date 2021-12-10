1a7174fdd60e2595c61f8f5b8c972488ad75d989
Nazeeh Maged <nazeehmaged007@gmail.com>
2021-12-10 02:19:13 +0200

    Fixed bug

--- XML_Editor/XML_Editor/Consistency.cs
+++ XML_Editor/XML_Editor/Consistency.cs
@@ -86,6 +86,7 @@ namespace XML_Editor
                                 st.Pop();
                                 errors++;
                             }
+                            output += "<" + "/" + st.Peek() + ">" + "\n";
                             st.Pop();
 
                         }
@@ -158,7 +159,6 @@ namespace XML_Editor
                     st.Pop();
                 }
             }
-            
             return output;
 
         }
